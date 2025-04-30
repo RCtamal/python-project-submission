36c23b61310a71a153dbc5e5346bc9a2e60fea05
RCtamal <121755598+RCtamal@users.noreply.github.com>
2025-04-30 23:35:06 +0530

    Add files via upload

---
+++ assignment-task-1.py
@@ -0,0 +1,24 @@
+#Task 1: Perform Basic Mathematical Operations
+
+""" number1 = ("Enter first number: ")
+number2 = ("Enter second number: ") """
+
+#Takes two numbers as input from the user
+# Converting the input strings to integers
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+# Performing basic mathematical operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print(number1, "and", number2)
+
+# Displaying the results of the operations
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
---
+++ assignment-task-2.py
@@ -0,0 +1,12 @@
+#Task 2: Create a Personalized Greeting
+
+
+#Takes a user's first name and last name as input
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+#Concatenates the first name and last name into a full name
+fullName = firstName + " " + lastName
+
+#Prints a personalized greeting message using the full name
+print("Hello " + fullName + "! Welcome to the world of Python programming.")
